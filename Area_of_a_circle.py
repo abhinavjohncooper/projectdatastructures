2c184e8a45091c05cbeb2e4a218f1e8c24ee8ea0
abhinavjohncooper <81818070+abhinavjohncooper@users.noreply.github.com>
2021-04-02 20:03:05 +0530

    Add files via upload

---
+++ Area_of_a_circle.py
@@ -0,0 +1,3 @@
+radius= float(input("Input the radius of a circle : "))
+area=3.141592653589793*(radius**2)
+print(f"The area of a circle with radius{radius} is :\n {area}")
---
+++ extension.py
@@ -0,0 +1,9 @@
+filename = input("Input the filename: ")
+if ".py" in filename:
+    print("The extension of the file is : 'python'")
+elif ".c" in filename:
+    print("The extension of the file is : 'C language'")
+elif ".java" in filename:
+    print("The extension of the file is : 'java'")
+else:
+    print("Error")            
